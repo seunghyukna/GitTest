7f8044a44612d3892fe7f59ea53be164a548e2b3
jinyoung-Kim0 <135565403+jinyoung-Kim0@users.noreply.github.com>
2023-08-11 12:35:51 +0900

    commit two_squashed
    asdf
    
    commit th
    
    wert23r

--- Assets/GitTest.cs
+++ Assets/GitTest.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class GitTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ASDF()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("HIHI");
     }
 }
